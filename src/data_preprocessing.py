6c94e8a6d209b821456546b3edd5c5d042c6ebc0
ESWAR <vardhaneswar213@gmail.com>
2025-11-14 11:32:42 -0500

    params yaml file configuted for the pipelime

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -6,6 +6,7 @@ from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 import string
 import nltk
+import yaml
 nltk.download('stopwords')
 nltk.download('punkt')
 
@@ -30,6 +31,25 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def transform_text(text):
     """
